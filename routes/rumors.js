096a5541ff77b7e472dc044f8f1ba5e9ecca6dd1
Steven Carroll <stevencarroll90@gmail.com>
2016-02-26 19:03:51 -0700

    Working copy

--- routes/rumors.js
+++ routes/rumors.js
@@ -1,10 +1,17 @@
 var express = require('express');
-var rumorStorage = require('../model/rumor-storage.js');
+
+var config = require('../helpers/config.js');
+var io = require('../server.js');
+var rumorStorage = require('../helpers/rumor-storage.js');
+var websocket = require('../helpers/websocket.js');
+
+console.log(io.sockets);
 
 var router = express.Router();
 
 router.post('/rumors', function(req, res) {
     rumorStorage.store(req.body);
+    websocket.receiveMessage();
     res.send({message: 'Rumor received and processed'});
 });
 
--- routes/wants.js
+++ routes/wants.js
@@ -1,14 +1,22 @@
 var express = require('express');
-var workQueue = require('../model/work-queue.js');
 var router = express.Router();
 
+var config = require('../helpers/config.js');
+var httpClient = require('../helpers/httpClient.js');
+var rumorStorage = require('../helpers/rumor-storage.js');
+var workQueue = require('../helpers/work-queue.js');
+
 router.post('/wants', function(req, res) {
-    console.log(req.body);
 
     workQueue.enqueue(req.body);
     while (!workQueue.empty()) {
         var want = workQueue.dequeue();
+	var requestedRumors = rumorStorage.getRumorsFromWant(want);
 	var url = want.EndPoint;
+	for (var i = 0; i < requestedRumors.length; i++) {
+	    var requestedRumor = {Rumor: requestedRumors[i], EndPoint: config.getBaseUrl()};
+            httpClient.send(url + '/wants', requestedRumor);
+	}
     }
 
     res.send({message: 'Want received and processed'});
