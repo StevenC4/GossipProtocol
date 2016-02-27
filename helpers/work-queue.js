905e1f65247c513d42a35ac946f8df717b2538dc
Steven Carroll <stevencarroll90@gmail.com>
2016-02-26 19:02:12 -0700

    Added a work queue. Ignoring swp files.

---
+++ helpers/work-queue.js
@@ -0,0 +1,21 @@
+var workQueueFunctions = {};
+
+var queue = [];
+
+workQueueFunctions.enqueue = function(data) {
+    queue.push(data);
+}
+
+workQueueFunctions.dequeue = function() {
+    if (queue.length > 0) {
+        return queue.shift();
+    } else {
+        return null;
+    }
+}
+
+workQueueFunctions.empty = function() {
+    return queue.length <= 0;
+}
+
+module.exports = workQueueFunctions;
